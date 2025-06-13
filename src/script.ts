32a6160fe36dda8f9f271658c4eba245677c10c9
eduabreu83 <eduardoabreu83@gmail.com>
2025-06-13 17:11:06 -0300

    at

--- src/script.ts
+++ src/script.ts
@@ -2,5 +2,21 @@ import z from "zod";
 
 const pattern = z.string();
 
-const result = pattern.parse("hello")
+const result = pattern.parse("eduardo")
 console.log(result) // true
+
+
+const person = z.object({
+    name: z.string().min(2).max(50),})
+
+    const employee = z.object({
+    role: z.string().min(2).max(50),})
+
+    const personWithEmployee = z.intersection(person, employee);
+
+    const result2 = personWithEmployee.safeParse({
+        name: 'John Doe',
+        role: 'Software Engineer'
+    });
+
+    console.log(result2); // true
